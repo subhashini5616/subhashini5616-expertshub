7221c21c375058771c0cdf185435d2ec1bac7fb9
subhashini5616 <93314025+subhashini5616@users.noreply.github.com>
2021-10-28 08:35:44 +0000

    Add files via upload

---
+++ ARITHMET.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   int no1, no2, no3, out1,o2,o3,o4,o5;
+   clrscr();
+   printf("Enter input values:");
+   scanf("%d%d",&no1,&no2);
+   //printf("Enter the value of b:");
+   //scanf("%d",&input2);
+   out1=no1+no2;
+   printf("sum of values are %d\n",out1);
+   o2=no1-no2;
+   printf("sub of values are %d\n",o2);
+   o3=no1*no2;
+   printf("multiply of values are %d\n",o3);
+   o4=no1/no2;
+   printf("division of values are %d\n",o4);
+   o5=no1%no2;
+   printf("modulus of values are %d\n",o5);
+   getch();
+}
